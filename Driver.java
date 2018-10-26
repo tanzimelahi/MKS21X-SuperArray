39c9b089868d8ce63966f2a8ece2e1be490cc653
tanzimelahi <telahi00@stuy.edu>
2018-10-25 23:22:19 -0400

    the complete superArray

--- Driver.java
+++ Driver.java
@@ -7,16 +7,51 @@ public class Driver {
     		  a.add("aba");
     	  }
     	  //first phase
-    	  System.out.println("should print 9 abas");
-    	  System.out.println(a);
-    	  System.out.println("shoud also print 9 abas plus one null");
-    	  System.out.println(a.toStringDebug());
-    	  System.out.println("should print one aba");
-    	  System.out.println(a.get(7));
-    	  a.set(6,"suraj");
-    	  System.out.println("should print suraj");
-    	  System.out.println(a.get(6));
+    	 System.out.println("should print 9 abas");
+    	 System.out.println(a);
+    	 System.out.println("shoud also print 9 abas plus one null");
+    	 System.out.println(a.toStringDebug());
+    	 System.out.println("should print one aba");
+    	 System.out.println(a.get(7));
+    	 a.set(6,"suraj");
+    	 System.out.println("should print suraj");
+    	 System.out.println(a.get(6));
+    	 
     	  //second phase
+    	  a.add("new");
+    	  System.out.println("checks to see if the resize method works,should add the element new and test to the list");
+    	  a.add("test");
+    	  System.out.println(a);
+    	  System.out.println("should print the list as seen above plus nine nulls");
+    	  System.out.println(a.toStringDebug());
+    	  System.out.println("should print true");
+    	  System.out.println(a.contains("new"));
+    	  System.out.println("should print false");
+    	  System.out.println(a.contains("kill"));
+    	  //time to test the overloaded add method
+    	  a.add(9,"goku");
+    	  System.out.println("should make goku to be the 9th element and push others to the right");
+    	  System.out.println(a);
+    	  System.out.println("should return 9");
+    	  System.out.println(a.indexOf("goku"));
+    	  System.out.println("should return -1");
+    	  System.out.println(a.indexOf("naruto"));
+    	  a.add(2,"goku");
+    	  System.out.println("should print 10");
+    	  System.out.println(a.lastIndexOf("goku"));
+    	  System.out.println("should print -1");
+    	  System.out.println(a.lastIndexOf("gok"));
+    	  System.out.println("should print goku");
+    	  System.out.println(a.remove(10));
+    	  System.out.println("should move other elements to the left");
+    	  System.out.println(a);
+    	  System.out.println("should return false");
+    	  System.out.println(a.remove("gok"));
+    	  a.add("goku");
+    	  a.remove("goku");
+    	  System.out.println("should remove the first instance of goku and shift all the elements to the left");
+    	  System.out.println(a);
+    	  
       }
 }
 
--- SuperArray.java
+++ SuperArray.java
@@ -23,6 +23,9 @@ public class SuperArray {
 		   }
 	   }
 	   public boolean add(String element) {
+		   if (size==data.length) {
+			   this.resize();
+		   }
 		   data[size]=element;
 		   size++;
 		   return true;
@@ -75,8 +78,76 @@ public class SuperArray {
 		   }
 		   this.data=newdata;
 	   }
+		public boolean contains(String element)  {
+			for(int i=0;i<size;i++) {
+				if(data[i].equals(element)) {
+					return true;
+				}
+			}return false;
+		}
+	   public void add(int index,String element) {
+		   if(index<0 || index>=size) {
+			   System.out.println("error:index of out bound exception");
+		   }
+		   else {
+			   this.size++;
+			   this.resize();//incase there isn't enough space in the list
+			   String[]newdata=new String[data.length];
+			   for(int i=0;i<index;i++) {
+				   newdata[i]=data[i];
+			   }
+			   newdata[index]=element;
+			   for(int i=index+1;i<newdata.length;i++) {
+				   newdata[i]=data[i-1];
+			   }
+			   data=newdata;
+		   }
+	   }
+	   public int indexOf(String target) {
+		   for(int i=0;i<size;i++) {
+			   if(data[i].equals(target)) {
+				   return i;
+			   }
+		   }return -1;
 		   
+	   }
+	   public int lastIndexOf(String target) {
+		   int result=-1;
+		   for(int i=0;i<size;i++) {
+			   if(data[i].equals(target)) {
+				   result=i;
+			   }
+		   }return result;
+	   }
+	   public String remove(int index) {
+		   if(index<0 || index>=size) {
+			   System.out.println("error:index of out bound exception");
+			   return "";
+		   }
+		   else {
+			   this.size=size-1;
+			   String[]newdata=new String[data.length];
+			   for(int i=0;i<index;i++) {
+				   newdata[i]=data[i];
+			   }
+			   String result=data[index];
+			   for(int i=index;i<newdata.length-1;i++) {
+				   newdata[i]=data[i+1];
+			   }
+			   data=newdata;
+			   return result;
+	   }
+    }
+	 public boolean remove(String element) {
+		 int index;
+		 for(int i=0;i<size;i++) {
+			 if (data[i].equals(element)){
+				 index=i;
+				 this.remove(i);
+				 return true;
+			 }
+		 }return false;
+	 }
 	   
-		   
 }
 
